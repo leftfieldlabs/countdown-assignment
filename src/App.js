76512a6660582cfb3e01829b3a2ed694f07593c9
geedtd <grcazares@gmail.com>
2023-09-22 04:23:53 -0700

    added date choosing functionality in App.js, conditionaly rendering Calendar.jsx and CountDownFunc.jsx based on wether a date has been chosen or not

--- src/App.js
+++ src/App.js
@@ -3,26 +3,42 @@ import './style/Countdown.css';
 
 import CountdownFunc from './components/CountdownFunc';
 import { Canvas } from '@react-three/fiber';
+import { useState } from 'react';
+import Calendar from './components/Calendar';
 
 
 function App() {
-  return (
-    <div className="App">
-      
-      <div className="content">
-        <Canvas
-          camera={{
-            fov:45,
-            near: 0.1,
-            far: 40,
-            position: [ 0, 0, 18]
-          }}
-        >
-          <CountdownFunc date='2023-09-23T18:34:30'/>
-        </Canvas>
-      </div>
-    </div>
-  );
+
+  const [selectedDate, setSelectedDate ] = useState(new Date());
+  const [dateChanged, setDateChanged] = useState(false)
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+    setDateChanged(true);
+  }
+
+  console.log(dateChanged)
+
+  if( dateChanged ) {
+    return (
+        <div className="content">
+          <Canvas
+            camera={{
+              fov:45,
+              near: 0.1,
+              far: 40,
+              position: [ 0, 0, 18]
+            }}
+          >
+            <CountdownFunc date={selectedDate} dateChanged={dateChanged}/>
+          </Canvas>
+        </div>
+    );
+  } else {
+    return (
+      <Calendar onDateChange={handleDateChange}/>
+    )
+  }
 }
 
 export default App;
